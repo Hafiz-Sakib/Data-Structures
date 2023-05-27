0d2c86c99d3cac52dc8a661be7afeca27860795b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-27 21:23:53 +0600

    odd even from array

--- Class-01(27-5)/a.c
+++ Class-01(27-5)/a.c
@@ -7,6 +7,11 @@ int main()
     int a[n];
     for (int i = 0; i < n; i++)
     {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
         if (a[i] % 2 == 0)
         {
             printf("Even Number");
