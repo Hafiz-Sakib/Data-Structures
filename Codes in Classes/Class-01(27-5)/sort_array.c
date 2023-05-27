ec2ca7546d147345c0538ca7b83c157596ee68c6
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-05-27 21:41:51 +0600

    Readme File

---
+++ Codes in Classes/Class-01(27-5)/sort_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, tmp = 0;
+    scanf("%d", &n);
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = (i + 1); j < n; j++)
+        {
+            if (a[i] > a[j])
+            {
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# <h1 align="center"> Data Structure <h1>
+
+## <h1 align="center"> Premier University,Chittagong <h1>
+
+### <h1 align="center"> Course Instructor : Adiba Ibnat Chowdhury <h1>
+
+<p align="center"> 
+<img height="100%" width="100%" src="https://i.postimg.cc/V6DkvWsK/codeforces-sponsored-by-ton.png">
+</p>
