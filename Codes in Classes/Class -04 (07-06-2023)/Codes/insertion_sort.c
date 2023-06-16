5cf75137e526ee4431a559400ded08ebe203b1ea
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-06-16 23:33:43 +0600

    Insertion Sort

---
+++ Codes in Classes/Class -04 (07-06-2023)/Codes/insertion_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ Codes in Classes/Class -04 (07-06-2023)/Codes/insertion_sort.py
@@ -0,0 +1,15 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+for i in range(1, n):
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > key:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = key
+
+for i in range(n):
+    print(a[i], end=" ")
+
+print()
