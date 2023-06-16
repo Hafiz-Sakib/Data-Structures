dce7c76abb8948d9e2e3eb084329e077238f4241
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-06-16 23:35:24 +0600

    Insertion Sort

---
+++ Codes in Classes/Class -04 (07-06-2023)/Codes/insertion_sort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class insertion_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 1; i < n; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && a[j] > key) {
+                a[j + 1] = a[j];
+                j--;
+            }
+            a[j + 1] = key;
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
