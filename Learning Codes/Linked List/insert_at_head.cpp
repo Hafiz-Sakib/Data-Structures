1da57e6aa2c313a1c34fc3ee0337e8616d94e392
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-08-08 23:53:16 +0600

    Linked List

---
+++ Learning Codes/Linked List/insert_at_head.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insert_at_head(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
+}
+void insert_at_tail(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+    node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = n;
+}
+void display(node *head)
+{
+    node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->data << " --> ";
+        tmp = tmp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insert_at_tail(head, 1);
+    insert_at_tail(head, 10);
+    insert_at_tail(head, 100);
+    insert_at_tail(head, 1000);
+    display(head);
+    insert_at_head(head, 2);
+    display(head);
+    return 0;
+}
