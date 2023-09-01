34a84c51c278f891b7afdaad1ca301171e50040e
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-01 19:32:41 +0600

    Changed

--- Learning Codes/Linked List/Mine/Python/arnab.py
+++ Learning Codes/Linked List/Mine/Python/arnab.py
@@ -7,8 +7,9 @@ class LinkedList:
     def __init__(self):
         self.head = None
     
-    def insetAtHead(self, value):
-        newNode = Node(value)
+    def insertAtHead(self, value):
+        newNode = Node()
+        newNode.linked_list(value)
         newNode.next = self.head
         self.head = newNode
 
@@ -17,7 +18,8 @@ class LinkedList:
             self.head = self.head.next
     
     def insertAtTail(self, value):
-        newNode = Node(value)
+        newNode = Node()
+        newNode.linked_list(value)
 
         if self.head is None:
             self.head = newNode
@@ -29,18 +31,22 @@ class LinkedList:
         current.next = newNode
 
     def deleteFromTail(self):
+        if self.head is None:
+            return
+
+        if self.head.next is None:
+            self.head = None
+            return
+
         current = self.head
         while current.next.next:
             current = current.next
         current.next = None
 
     def insertAtAnyPos(self, pos, value):
-        newNode = Node(value)
+        newNode = Node()
+        newNode.linked_list(value)
 
-        if self.head == None:
-            self.head = newNode
-            return
-        
         if pos == 1:
             newNode.next = self.head
             self.head = newNode
@@ -48,24 +54,35 @@ class LinkedList:
         
         current = self.head
         for i in range(pos - 2):
+            if current is None:
+                return
             current = current.next
+        if current is None:
+            return
         newNode.next = current.next
         current.next = newNode
     
     def deleteFromAnyPos(self, pos):
-        current = self.head
         if pos == 1:
-            self.head = self.head.next
+            if self.head:
+                self.head = self.head.next
+            return
+
+        current = self.head
         for i in range(pos - 2):
+            if current is None:
+                return
             current = current.next
+        if current is None or current.next is None:
+            return
         current.next = current.next.next
 
     def search(self, key):
         pos = 1
         current = self.head
-        while current.next:
+        while current:
             if current.data == key:
-                print("Founded, position No: ", pos)
+                print("Found, position No:", pos)
                 return
             current = current.next
             pos += 1
@@ -78,24 +95,22 @@ class LinkedList:
             current = current.next
         print("None")
 
-
-
-list = LinkedList()
-list.insetAtHead(5)
-list.insetAtHead(7)
-list.display()
-list.insertAtTail(1)
-list.insertAtTail(2)
-list.display()
-list.insertAtAnyPos(3, 8)
-list.insertAtAnyPos(1, 9)
-list.insertAtAnyPos(7, 4)
-list.display()
-list.deleteFromHead()
-list.display()
-list.deleteFromTail()
-list.display()
-list.deleteFromAnyPos(3)
-list.display()
-list.search(3)
-
+# Create a linked list and perform operations
+lst = LinkedList()
+lst.insertAtHead(5)
+lst.insertAtHead(7)
+lst.display()
+lst.insertAtTail(1)
+lst.insertAtTail(2)
+lst.display()
+lst.insertAtAnyPos(3, 8)
+lst.insertAtAnyPos(1, 9)
+lst.insertAtAnyPos(7, 4)
+lst.display()
+lst.deleteFromHead()
+lst.display()
+lst.deleteFromTail()
+lst.display()
+lst.deleteFromAnyPos(3)
+lst.display()
+lst.search(3)
