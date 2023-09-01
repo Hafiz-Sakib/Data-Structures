d633a2b77ece5257a933f07f7da5904f0af19a7b
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-01 22:08:28 +0600

    Code Updates

---
+++ Lab Report/DS Lab Report 02/Source Codes/problem03.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.data = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insertAtAnyPos(self, pos, value):
+        newNode = Node(value)
+
+        if self.head == None:
+            self.head = newNode
+            return
+        
+        if pos == 1:
+            newNode.next = self.head
+            self.head = newNode
+            return
+        
+        current = self.head
+        for i in range(pos - 2):
+            current = current.next
+        newNode.next = current.next
+        current.next = newNode
+
+
+
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+
+
+list = LinkedList()
+list.insertAtAnyPos(3, 25)
+list.insertAtAnyPos(1, 50)
+list.insertAtAnyPos(2, 75)
+list.display()
