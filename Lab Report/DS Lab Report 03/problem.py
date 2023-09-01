b7f97475477fb1f6fba0f551dd70b15132e70164
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2023-09-01 22:10:22 +0600

    Code Updates

---
+++ Lab Report/DS Lab Report 03/problem.py
@@ -0,0 +1,27 @@
+def postfix(expression):
+    stack= []
+    result = 0
+    for char in expression:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            oparand1 = stack.pop()
+            oparand2 = stack.pop()
+
+            if char == '+':
+                result = oparand1 + oparand2
+            elif char == '-':
+                result = oparand1 - oparand2
+            elif char == '*':
+                result = oparand1 * oparand2
+            elif char == '/':
+                result = oparand1 / oparand2
+
+            stack.append(result)
+        
+    return stack[0]
+
+
+
+result = postfix('23*45+*')
+print(result)
